82f4de7ec362f0bf2779c78881e945904a7a1a5b
Maury Areas <mareasperez7@gmail.com>
2021-03-31 17:55:19 -0600

    Tarea = add tarea.estado: alter

--- src/components/Tarea.vue
+++ src/components/Tarea.vue
@@ -9,21 +9,46 @@
             <div class="input-group">
               <input
                 type="text"
+                v-model="tarea"
                 class="form-control form-control-lg"
                 placeholder="Agregar Tarea"
               />
               <div class="input-group-append">
-                <button class="btn btn-success btn-lg">Agregar</button>
+                <button
+                  class="btn btn-success btn-lg"
+                  v-on:click="agregarTarea()"
+                >
+                  Agregar
+                </button>
               </div>
             </div>
             <br />
+            <h5 v-if="listTareas.length == 0">
+              No hay tareas para mostrar
+            </h5>
+            <!-- {{ listTareas }} -->
             <ul class="list-group">
-              <li class="list-group-item d-flex justify-content-between">
-                <span class="text-success cursor">
-                  <i class="fal fa-circle"></i>
+              <li
+                v-for="(tarea, index) of listTareas"
+                :key="index"
+                class="list-group-item d-flex justify-content-between"
+              >
+                <span
+                  class="cursor"
+                  v-bind:class="{ 'text-success': tarea.estado }"
+                  ><i
+                    v-bind:class="[
+                      tarea.estado ? 'fas fa-check-circle' : 'fal fa-circle',
+                    ]"
+                    v-on:click="editarEstado(tarea, index)"
+                  ></i>
                 </span>
-                hacer la tarea
-                <span class="text-danger cursor">
+                <!-- hacer la tarea -->
+                {{ tarea.nombre }}
+                <span
+                  class="text-danger cursor"
+                  v-on:click="eliminarTarea(index)"
+                >
                   <i class="far fa-trash-alt"></i>
                 </span>
               </li>
@@ -38,6 +63,28 @@
 <script>
 export default {
   name: "Tarea",
+  data() {
+    return {
+      tarea: "",
+      listTareas: [],
+    };
+  },
+  methods: {
+    agregarTarea() {
+      const Tarea = {
+        nombre: this.tarea,
+        estado: false,
+      };
+      this.listTareas.push(Tarea);
+      this.tarea = "";
+    },
+    eliminarTarea(index) {
+      this.listTareas.splice(index, 1);
+    },
+    editarEstado(tarea, index) {
+      this.listTareas[index].estado = !tarea.estado;
+    },
+  },
 };
 </script>
 
